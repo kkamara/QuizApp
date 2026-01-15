580bdde4d115baaaa563ad6efcdb7420adca5c7f
kkamara <kamaracomputers@gmail.com>
2026-01-15 16:54:33 +0000

    Navigate to questions screen and prepare questions

--- app/src/main/java/com/kelvinkamara/quizapp/MainActivity.kt
+++ app/src/main/java/com/kelvinkamara/quizapp/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.kelvinkamara.quizapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.kelvinkamara.quizapp.ui.QuestionsActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +20,24 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val startButton: Button = findViewById(R.id.button_start)
+        val editTextName: EditText = findViewById(R.id.name)
+
+        startButton.setOnClickListener {
+            if (editTextName.text.isNotEmpty()) {
+                Intent(this@MainActivity, QuestionsActivity::class.java).also {
+                    startActivity(it)
+                    finish() // Stop back button going back in history.
+                }
+            } else {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Please enter your name.",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
         }
     }
 }
---
+++ app/src/main/java/com/kelvinkamara/quizapp/utils/Constants.kt
@@ -0,0 +1,93 @@
+package com.kelvinkamara.quizapp.utils
+
+import com.kelvinkamara.quizapp.model.Question
+import com.kelvinkamara.quizapp.R
+
+object Constants {
+    fun getQuestions(): MutableList<Question> {
+        val questions = mutableListOf<Question>()
+
+        val quest1 = Question(
+            1,"Which country does this flag belong to?",
+            R.drawable.italy_flag, "Italy",
+            "India", "Iran",
+            "Ireland", 1
+        )
+        questions.add(quest1)
+
+
+        val quest2 = Question(
+            1,"Which country does this flag belong to?",
+            R.drawable.india_flag, "Ireland",
+            "Germany", "India",
+            "Finland", 3
+        )
+        questions.add(quest2)
+
+        val quest3 = Question(
+            1,"Which country does this flag belong to?",
+            R.drawable.spain_flag, "Spain",
+            "Italy", "France",
+            "Brazil", 1
+        )
+        questions.add(quest3)
+
+        val quest4 = Question(
+            1,"Which country does this flag belong to?",
+            R.drawable.brazil_flag, "England",
+            "France", "Argentina",
+            "Brazil", 4
+        )
+        questions.add(quest4)
+
+        val quest5 = Question(
+            1,"Which country does this flag belong to?",
+            R.drawable.finland_flag, "Scotland",
+            "Italy", "Finland",
+            "France", 3
+        )
+        questions.add(quest5)
+
+        val quest6 = Question(
+            1,"Which country does this flag belong to?",
+            R.drawable.germany_flag, "Argentina",
+            "Romania", "Germany",
+            "Spain", 3
+        )
+        questions.add(quest6)
+
+        val quest7 = Question(
+            1,"Which country does this flag belong to?",
+            R.drawable.romania_flag, "Romania",
+            "Romania", "Ireland",
+            "Italy", 1
+        )
+        questions.add(quest7)
+
+        val quest8 = Question(
+            1,"Which country does this flag belong to?",
+            R.drawable.nigeria_flag, "Nigeria",
+            "India", "Wales",
+            "Italy", 1
+        )
+        questions.add(quest8)
+
+        val quest9 = Question(
+            1,"Which country does this flag belong to?",
+            R.drawable.ireland_flag, "Argentina",
+            "Ireland", "Scotland",
+            "Romania", 2
+        )
+        questions.add(quest9)
+
+        val quest10 = Question(
+            1,"Which country does this flag belong to?",
+            R.drawable.argentina_flag, "England",
+            "Brazil", "France",
+            "Argentina", 4
+        )
+        questions.add(quest10)
+
+        return questions
+    }
+}
