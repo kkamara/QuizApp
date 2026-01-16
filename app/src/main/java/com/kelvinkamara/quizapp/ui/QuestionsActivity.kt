bc12da53cbae8c931dfaa242d5082de010072bbf
kkamara <kamaracomputers@gmail.com>
2026-01-16 12:45:58 +0000

    Design and logic for question options

--- app/src/main/java/com/kelvinkamara/quizapp/ui/QuestionsActivity.kt
+++ app/src/main/java/com/kelvinkamara/quizapp/ui/QuestionsActivity.kt
@@ -1,19 +1,24 @@
 package com.kelvinkamara.quizapp.ui
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.kelvinkamara.quizapp.R
 import com.kelvinkamara.quizapp.model.Question
 import com.kelvinkamara.quizapp.utils.Constants
 
-class QuestionsActivity : AppCompatActivity() {
+class QuestionsActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var progressBar: ProgressBar
     private lateinit var textViewProgress: TextView
     private lateinit var textViewQuestion: TextView
@@ -23,8 +28,12 @@ class QuestionsActivity : AppCompatActivity() {
     private lateinit var textViewOptionTwo: TextView
     private lateinit var textViewOptionThree: TextView
     private lateinit var textViewOptionFour: TextView
+
+    private lateinit var checkButton: Button
+
     private val currentPosition = 1
     private lateinit var questionsList: MutableList<Question>
+    private var selectedOptionPosition = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,11 +49,18 @@ class QuestionsActivity : AppCompatActivity() {
         textViewProgress = findViewById(R.id.text_view_progress)
         textViewQuestion = findViewById(R.id.question_text_view)
         flagImage = findViewById(R.id.image_flag)
+        checkButton = findViewById(R.id.button_check)
 
         textViewOptionOne = findViewById(R.id.text_view_option_one)
         textViewOptionTwo = findViewById(R.id.text_view_option_two)
         textViewOptionThree = findViewById(R.id.text_view_option_three)
         textViewOptionFour = findViewById(R.id.text_view_option_four)
+
+        textViewOptionOne.setOnClickListener(this)
+        textViewOptionTwo.setOnClickListener(this)
+        textViewOptionThree.setOnClickListener(this)
+        textViewOptionFour.setOnClickListener(this)
+        checkButton.setOnClickListener {  }
 
         questionsList = Constants.getQuestions()
         Log.d("QuestionSize", "${questionsList.size}")
@@ -62,5 +78,61 @@ class QuestionsActivity : AppCompatActivity() {
         textViewOptionTwo.text = question.optionTwo
         textViewOptionThree.text = question.optionThree
         textViewOptionFour.text = question.optionFour
+
+        if (currentPosition == questionsList.size) {
+            checkButton.text = "FINISH"
+        } else {
+            checkButton.text = "CHECK"
+        }
+    }
+
+    private fun resetOptions() {
+        val options = mutableListOf<TextView>()
+        options.add(textViewOptionOne)
+        options.add(textViewOptionTwo)
+        options.add(textViewOptionThree)
+        options.add(textViewOptionFour)
+
+        for (option in options) {
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(
+                this,
+                R.drawable.default_option_border_bg
+            )
+        }
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id) {
+            R.id.text_view_option_one -> {
+                selectedOption(textViewOptionOne, 1)
+            }
+            R.id.text_view_option_two -> {
+                selectedOption(textViewOptionTwo, 2)
+            }
+            R.id.text_view_option_three -> {
+                selectedOption(textViewOptionThree, 3)
+            }
+            R.id.text_view_option_four -> {
+                selectedOption(textViewOptionFour, 4)
+            }
+            R.id.button_check -> {
+
+            }
+        }
+    }
+
+    private fun selectedOption(textView: TextView, selectedOptionNumber: Int) {
+        resetOptions()
+
+        selectedOptionPosition = selectedOptionNumber
+
+        textView.setTextColor(Color.parseColor("#363A43"))
+        textView.setTypeface(textView.typeface, Typeface.BOLD)
+        textView.background = ContextCompat.getDrawable(
+            this,
+            R.drawable.selected_option_border_bg
+        )
     }
 }
